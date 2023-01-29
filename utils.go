bb9830ab444ea3488b0b7a653de832aaf986f11d
Niexiawei <1135215907@qq.com>
2023-01-29 15:09:34 +0800

    add StringToBytes and BytesToString

--- utils.go
+++ utils.go
@@ -1,9 +1,28 @@
 package golangutils
 
+import (
+	"reflect"
+	"unsafe"
+)
+
 func If[T any](condition bool, trueVal, falseVal T) T {
 	if condition {
 		return trueVal
 	} else {
 		return falseVal
 	}
+}
+
+func StringToBytes(s string) []byte {
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	bh := reflect.SliceHeader{
+		Data: sh.Data,
+		Len:  sh.Len,
+		Cap:  sh.Len,
+	}
+	return *(*[]byte)(unsafe.Pointer(&bh))
+}
+
+func BytesToString(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
 }
