998fc76eb49f0c7a6a0364252fa3e6ca0b93b86b
Niexiawei <niexiawei@nicoi.cn>
2023-10-28 15:55:32 +0800

    补全current_path测试用例

---
+++ pathtool/current_path_test.go
@@ -0,0 +1,11 @@
+package pathtool
+
+import "testing"
+
+func TestGetCurrentAbPathByExecutable(t *testing.T) {
+	t.Log(GetCurrentAbPathByExecutable())
+}
+
+func TestGetTmpDir(t *testing.T) {
+	t.Log(GetTmpDir())
+}
